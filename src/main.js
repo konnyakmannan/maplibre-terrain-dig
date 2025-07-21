8080fe2a6d23d116cd720baac5862a2c5613fdd3
Taiki Kanda <taiki@konnyak.com>
2025-07-21 18:00:11 +0900

    switch DEM data from Nagasaki to Kanagawa.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { makeNumPngDemProtocol }
 
 const map = new maplibregl.Map( {
 	container: 'map',					// コンテナ<div>要素のID
-	center: [ 129.84935, 32.75352 ],	// 中心点の[ 経度, 緯度 ], 長崎市稲佐山山頂
+	center: [ 139.246068, 35.427170 ],	// 中心点の[ 経度, 緯度 ], 大山
 	zoom: 13,							// ズームレベル
 	maxZoom: 18,
     style: {
@@ -18,12 +18,12 @@ const map = new maplibregl.Map( {
                 	+ '地理院タイル</a>',
                 maxzoom: 18
             }, 
-			nagasakidem: {
+			kanagawadem: {
                 type: 'raster-dem',
                 tiles: [
-                    'numpngdem://forestgeo.info/opendata/42_nagasaki/dem_2022/{z}/{x}/{y}.png'
+                    'numpngdem://forestgeo.info/opendata/14_kanagawa/dem_2022/{z}/{x}/{y}.png'
                 ],
-				attribution: '<a href="https://www.geospatial.jp/ckan/dataset/rinya-nagasaki-maptiles">長崎県・DEM（PNG標高タイル/2022）林野庁加工</a>',
+				attribution: '<a href="https://www.geospatial.jp/ckan/dataset/rinya-kanagawa-maptiles2">神奈川県・DEM（PNG標高タイル/2022）林野庁加工</a>',
                 tileSize: 256,
                 maxzoom: 18,
             },
@@ -36,7 +36,7 @@ const map = new maplibregl.Map( {
             }
         ],
 		terrain: {
-            source: 'nagasakidem',
+            source: 'kanagawadem',
             exaggeration: 1
 		}
 	},
